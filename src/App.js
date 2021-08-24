7ede9f79aa4dbc16053227852419d5f81a2114a3
Your Name <you@example.com>
2021-08-25 04:36:10 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import BookSearch from "./component/BookSearch";
 import BookList from "./component/BookList";
 import * as BooksAPI from "./Api/BooksAPI";
 
-const BooksApp = ({}) => {
+const BooksApp = () => {
   const [books, setbooks] = useState([]);
   const [searchedBooks, setsearchedBooks] = useState([]);
-  const [filteredBooks, setfilteredBooks] = useState([]);
+
   const [query, setquery] = useState("");
   useEffect(() => {
     BooksAPI.getAll().then((books) => {
       setbooks(books);
     });
   }, []);
-
+  console.log(query);
   const changeShelf = (book, shelf) => {
     BooksAPI.update(book, shelf).then((response) => {
       book.shelf = shelf;
@@ -31,6 +31,7 @@ const BooksApp = ({}) => {
           if (searchedBooks.error) {
             setsearchedBooks([]);
           } else {
+            // eslint-disable-next-line array-callback-return
             searchedBooks.map((filteredBooks) => {
               let bookOnShelf = books.find((b) => b.id === filteredBooks.id);
 
